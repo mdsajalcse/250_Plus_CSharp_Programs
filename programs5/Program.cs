d24383b2fa2a2a7723c5a1eb89c2e7a003d7a159
ifthekar <sajalcse2018@gmail.com>
2023-09-19 16:48:05 +0600

    To Check maximum number between two numbers

---
+++ programs5/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace programs5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //To Check maximum number between two numbers
+           
+            Console.WriteLine("Enter First Number= ");
+            int num1=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second Number");
+            int num2=int.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                Console.WriteLine("Num1 is Greater");
+            }
+            else if (num1 == num2)
+            {
+                Console.WriteLine("Num1 is equal to num2");          }
+            else
+            {
+                Console.WriteLine("Num2 is Greater");
+            }
+        }
+    }
+}
