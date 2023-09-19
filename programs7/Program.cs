87dcae2b0c6b7b703ce7fe6cb7ca05a2ef2bbef4
ifthekar <sajalcse2018@gmail.com>
2023-09-19 17:59:21 +0600

    program to find area & perimeter from length & breadth

---
+++ programs7/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace programs7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            //write a program to find area & perimeter from length & breadth
+        {
+            Console.WriteLine("Enter the Length=");
+            double length=double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter The Breadth or Height= ");
+            double breadth=double.Parse(Console.ReadLine());
+
+            double perimeter = 2 * (length + breadth);
+            double area = length * breadth;
+
+            Console.WriteLine("Perimter is=" + perimeter);
+            Console.WriteLine("Area is= " + area);
+
+
+        }
+    }
+}
