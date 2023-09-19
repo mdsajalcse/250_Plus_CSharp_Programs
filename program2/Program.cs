e1492a4bc20143d9599e069d9fe5342f8312c5fd
ifthekar <sajalcse2018@gmail.com>
2023-09-19 15:57:30 +0600

    program2 added

---
+++ program2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace program2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, sum ,sub,mul,mod,div;
+            a = 10;
+            b = 20;
+            sum = a + b;
+            sub = b - a;
+            mul = a * b;
+            mod = b % a;
+            div=  b / a;
+            Console.WriteLine("Sum is::"+sum);
+            Console.WriteLine("Subtraction is::" + sub);
+            Console.WriteLine("Multiplication is::" + mul);
+            Console.WriteLine("Modulas value is::" + mod);
+            Console.WriteLine("Divison is::" + div);
+            Console.WriteLine("Greater Value from A & B::" +(a <= b)); //boolean condition
+            Console.WriteLine("Smaller Value from A & B::" + (a >= b));
+
+
+                     
+                
+                }
+    }
+}
