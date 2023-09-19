4693b94ae36ebffca2e867aafa3292f164b639fd
ifthekar <sajalcse2018@gmail.com>
2023-09-19 12:50:18 +0600

    program1-Hello world create

---
+++ program1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace program1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello C#");
+        }
+    }
+}
---
+++ programs1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace programs1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
