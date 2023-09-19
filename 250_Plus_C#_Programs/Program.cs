8c0789d72506d366f3139162baa57b41c6dcc00c
ifthekar <sajalcse2018@gmail.com>
2023-09-19 11:48:47 +0600

    Add project files.

---
+++ 250_Plus_C#_Programs/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+//250_Plus_C#_Programs
+//Find data types size
+
+int number =5;
+Console.WriteLine("Int Number is::" + number);
+Console.WriteLine("Size of Int is::" +sizeof(int));
+
+float number1 = 5.7f;
+Console.WriteLine("Float Number is::" + number1);
+Console.WriteLine("size of float data::" + sizeof(float));
+
+double number2 = 44.5879d;
+Console.WriteLine("Double Number is::" + number2);
+Console.WriteLine("size of double data type is::" + sizeof(double));
+
+char ch = 'h';
+Console.WriteLine("Char is::" + ch);
+Console.WriteLine("Size of Char is::"+sizeof(char));
+
+string sentence = "Hello World";
+Console.WriteLine("String is::" + sentence);
+//Console.WriteLine("Size of String is::" + sizeof(string));
+short numberS = 12;
+Console.WriteLine("Short NUmber is::" + numberS);
+Console.WriteLine("Short Data Size ::" + sizeof(short));
+
+long numberL = 123456789;
+Console.WriteLine("The long Value is::" + numberL);
+Console.WriteLine("Long Size is::" + sizeof(long));
+
+
+
+
+
+
+
+
+
+
